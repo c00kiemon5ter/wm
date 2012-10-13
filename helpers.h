05f5ab3d90009db84b9684b3b397c05e32bfa845
Ivan Kanakarakis <ivan.kanak@gmail.com>
2012-10-14 01:21:38 +0300

    helpers.h: formatting

--- helpers.h
+++ helpers.h
@@ -8,15 +8,16 @@
 #define XCB_CONFIG_WINDOW_X_Y_WIDTH_HEIGHT XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y | XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT
 #define XCB_CONFIG_WINDOW_X_Y XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y
 
-#define LENGTH(x)       (sizeof(x) / sizeof(*x))
-#define MAX(A, B)       ((A) > (B) ? (A) : (B))
-#define MIN(A, B)       ((A) < (B) ? (A) : (B))
-#define BOOLSTR(A)      ((A) ? "true" : "false")
+#define LENGTH(x)           (sizeof(x) / sizeof(*x))
+#define BOOLSTR(x)          ("false\0true" + 6 * x)
+
+#define MAX(A, B)           ((A) > (B) ? (A) : (B))
+#define MIN(A, B)           ((A) < (B) ? (A) : (B))
 
 #ifdef DEBUG
-#  define PRINTF(fmt,...) printf(":: %5d:%15.15s: " fmt, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define PRINTF(fmt,...)     printf(":: %5d:%15.15s: " fmt, __LINE__, __FUNCTION__, __VA_ARGS__)
 #else
-#  define PRINTF(fmt,...) ((void)0)
+#define PRINTF(fmt,...)     ((void)0)
 #endif
 
 void warn(char *, ...);
