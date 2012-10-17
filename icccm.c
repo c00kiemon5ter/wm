2b9f86dc52bcb0ec4057c45d8952ed845884264a
Ivan Kanakarakis <ivan.kanak@gmail.com>
2012-10-17 23:56:17 +0300

    icccm close window with WM_PROTOCOLS and WM_DELETE_WINDOW atom event

--- icccm.c
+++ icccm.c
@@ -6,6 +6,58 @@
 #include "helpers.h"
 #include "icccm.h"
 
+xcb_atom_t get_atom(const char *atom_name)
+{
+    xcb_atom_t atom = 0;
+
+    xcb_intern_atom_cookie_t cookie = xcb_intern_atom_unchecked(cfg.conn, 0, strlen(atom_name), atom_name);
+    xcb_intern_atom_reply_t *reply = xcb_intern_atom_reply(cfg.conn, cookie, (void *)0);
+
+    if (!reply)
+        return 0;
+
+    atom = reply->atom;
+    free(reply);
+
+    return atom;
+}
+
+bool icccm_close_window(const xcb_window_t win)
+{
+    bool state = false;
+
+    xcb_atom_t wm_protocols = get_atom(WM_PROTOCOLS);
+    xcb_atom_t wm_delete_window = get_atom(WM_DELETE_WINDOW);
+
+    if (!wm_protocols || !wm_delete_window)
+        return state;
+
+    const xcb_get_property_cookie_t cookie = xcb_icccm_get_wm_protocols_unchecked(cfg.conn, win, wm_protocols);
+    xcb_icccm_get_wm_protocols_reply_t data;
+
+    if (!xcb_icccm_get_wm_protocols_reply(cfg.conn, cookie, &data, (void *)0))
+        return state;
+
+    for (unsigned short i = 0; i < data.atoms_len; i++)
+        if ((state = data.atoms[i] == wm_delete_window))
+            break;
+
+    xcb_icccm_get_wm_protocols_reply_wipe(&data);
+
+    xcb_client_message_event_t evt = {
+        .response_type = XCB_CLIENT_MESSAGE,
+        .window = win,
+        .format = 32,
+        .type = wm_protocols,
+        .data.data32[0] = wm_delete_window,
+        .data.data32[1] = XCB_CURRENT_TIME,
+    };
+
+    xcb_send_event(cfg.conn, false, win, XCB_EVENT_MASK_NO_EVENT, (char *)&evt);
+
+    return state;
+}
+
 bool icccm_get_window_name(const xcb_window_t win, char *name)
 {
     const xcb_get_property_cookie_t cookie = xcb_icccm_get_wm_name_unchecked(cfg.conn, win);
--- icccm.h
+++ icccm.h
@@ -5,6 +5,10 @@
 
 #include <xcb/xcb.h>
 
+#define WM_PROTOCOLS        "WM_PROTOCOLS"
+#define WM_DELETE_WINDOW    "WM_DELETE_WINDOW"
+
+bool icccm_close_window(const xcb_window_t);
 bool icccm_get_window_name(const xcb_window_t, char *);
 bool icccm_is_transient(const xcb_window_t);
 
