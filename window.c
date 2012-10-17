00076b2d9c155096188171209bc877fb8e434683
Ivan Kanakarakis <ivan.kanak@gmail.com>
2012-10-17 12:37:15 +0300

    rearrange order of functions in window

--- window.c
+++ window.c
@@ -11,115 +11,9 @@
 
 #define WINDOW_NO_NAME     "no name"
 
-void window_set_visibility(const xcb_window_t win, bool visible)
-{
-    uint32_t values_off[] = { ROOT_EVENT_MASK & ~XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY };
-    uint32_t values_on[]  = { ROOT_EVENT_MASK };
-    xcb_change_window_attributes(cfg.conn, cfg.screen->root, XCB_CW_EVENT_MASK, values_off);
-    if (visible)
-        xcb_map_window(cfg.conn, win);
-    else
-        xcb_unmap_window(cfg.conn, win);
-    xcb_change_window_attributes(cfg.conn, cfg.screen->root, XCB_CW_EVENT_MASK, values_on);
-}
-
-inline void window_hide(const xcb_window_t win)
-{
-    window_set_visibility(win, false);
-}
-
-inline void window_show(const xcb_window_t win)
-{
-    window_set_visibility(win, true);
-}
-
-void window_set_border_width(xcb_window_t win, const uint16_t border_width)
-{
-    const uint32_t values[] = { border_width };
-    xcb_configure_window(cfg.conn, win, XCB_CONFIG_WINDOW_BORDER_WIDTH, values);
-}
-
-void window_move(const xcb_window_t win, const int16_t x, const int16_t y)
-{
-    const uint32_t values[] = { x, y };
-    xcb_configure_window(cfg.conn, win, XCB_CONFIG_WINDOW_MOVE, values);
-}
-
-void window_resize(const xcb_window_t win, const uint16_t width, const uint16_t height)
-{
-    const uint32_t values[] = { width, height };
-    xcb_configure_window(cfg.conn, win, XCB_CONFIG_WINDOW_RESIZE, values);
-}
-
-void window_move_resize(const xcb_window_t win, const int16_t x, const int16_t y, const uint16_t width, const uint16_t height)
-{
-    const uint32_t values[] = { x, y, width, height };
-    xcb_configure_window(cfg.conn, win, XCB_CONFIG_WINDOW_MOVE_RESIZE, values);
-}
-
-void window_move_resize_geom(const xcb_window_t win, const xcb_rectangle_t geom)
-{
-    const uint32_t values[] = { geom.x, geom.y, geom.width, geom.height };
-    xcb_configure_window(cfg.conn, win, XCB_CONFIG_WINDOW_MOVE_RESIZE, values);
-}
-
-void toggle_fullscreen(client_t *c)
-{
-    c->is_fullscrn = !c->is_fullscrn;
-    xcb_atom_t values[] = { c->is_fullscrn ? cfg.ewmh->_NET_WM_STATE_FULLSCREEN : XCB_NONE };
-    xcb_ewmh_set_wm_state(cfg.ewmh, c->win, LENGTH(values), values);
-
-    if (c->is_fullscrn) {
-        window_set_border_width(c->win, 0);
-        window_move_resize_geom(c->win, c->mon->geom);
-    }
-}
-
-bool window_override_redirect(const xcb_window_t win)
-{
-    bool state = false;
-
-    const xcb_get_window_attributes_cookie_t cookie = xcb_get_window_attributes(cfg.conn, win);
-    xcb_get_window_attributes_reply_t *reply = xcb_get_window_attributes_reply(cfg.conn, cookie, (void *)0);
-
-    if (!reply)
-        return state;
-
-    state = reply->override_redirect;
-
-    free(reply);
-
-    return state;
-}
-
-bool window_update_geom(const xcb_window_t win, xcb_rectangle_t *geom)
-{
-    const xcb_get_geometry_cookie_t cookie = xcb_get_geometry(cfg.conn, win);
-    xcb_get_geometry_reply_t *reply = xcb_get_geometry_reply(cfg.conn, cookie, (void *)0);
-
-    if (!reply)
-        return false;
-
-    geom->x = reply->x;
-    geom->y = reply->y;
-    geom->width = reply->width;
-    geom->height = reply->height;
-
-    free(reply);
-
-    return true;
-}
-
-void client_set_geom(client_t *c, const int16_t x, const int16_t y, const uint16_t w, const uint16_t h)
-{
-    window_move_resize(c->win, c->geom.x = x, c->geom.y = y, c->geom.width = w, c->geom.height = h);
-}
-
-void client_set_geom_geom(client_t *c, const xcb_rectangle_t geom)
-{
-    window_move_resize_geom(c->win, c->geom = geom);
-}
-
+/**
+ * initialize allocate and return the new client
+ */
 client_t *create_client(const xcb_window_t win)
 {
     PRINTF("creating client for window: %u\n", win);
@@ -174,5 +68,124 @@ client_t *locate(const xcb_window_t win)
 
     PRINTF("no client for window: %u\n", win);
     return (void *)0;
+}
+
+bool window_override_redirect(const xcb_window_t win)
+{
+    bool state = false;
+
+    const xcb_get_window_attributes_cookie_t cookie = xcb_get_window_attributes(cfg.conn, win);
+    xcb_get_window_attributes_reply_t *reply = xcb_get_window_attributes_reply(cfg.conn, cookie, (void *)0);
+
+    if (!reply)
+        return state;
+
+    state = reply->override_redirect;
+
+    free(reply);
+
+    return state;
+}
+
+void window_set_border_width(xcb_window_t win, const uint16_t border_width)
+{
+    const uint32_t values[] = { border_width };
+    xcb_configure_window(cfg.conn, win, XCB_CONFIG_WINDOW_BORDER_WIDTH, values);
+}
+
+/* ** visibility functions ** */
+
+void window_set_visibility(const xcb_window_t win, bool visible)
+{
+    uint32_t values_off[] = { ROOT_EVENT_MASK & ~XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY };
+    uint32_t values_on[]  = { ROOT_EVENT_MASK };
+    xcb_change_window_attributes(cfg.conn, cfg.screen->root, XCB_CW_EVENT_MASK, values_off);
+    if (visible)
+        xcb_map_window(cfg.conn, win);
+    else
+        xcb_unmap_window(cfg.conn, win);
+    xcb_change_window_attributes(cfg.conn, cfg.screen->root, XCB_CW_EVENT_MASK, values_on);
+}
+
+inline
+void window_hide(const xcb_window_t win)
+{
+    window_set_visibility(win, false);
+}
+
+inline
+void window_show(const xcb_window_t win)
+{
+    window_set_visibility(win, true);
+}
+
+/* ** window move and resize function ** */
+
+void window_move(const xcb_window_t win, const int16_t x, const int16_t y)
+{
+    const uint32_t values[] = { x, y };
+    xcb_configure_window(cfg.conn, win, XCB_CONFIG_WINDOW_MOVE, values);
+}
+
+void window_resize(const xcb_window_t win, const uint16_t width, const uint16_t height)
+{
+    const uint32_t values[] = { width, height };
+    xcb_configure_window(cfg.conn, win, XCB_CONFIG_WINDOW_RESIZE, values);
+}
+
+void window_move_resize(const xcb_window_t win, const int16_t x, const int16_t y, const uint16_t width, const uint16_t height)
+{
+    const uint32_t values[] = { x, y, width, height };
+    xcb_configure_window(cfg.conn, win, XCB_CONFIG_WINDOW_MOVE_RESIZE, values);
+}
+
+void window_move_resize_geom(const xcb_window_t win, const xcb_rectangle_t geom)
+{
+    const uint32_t values[] = { geom.x, geom.y, geom.width, geom.height };
+    xcb_configure_window(cfg.conn, win, XCB_CONFIG_WINDOW_MOVE_RESIZE, values);
+}
+
+/* ** client move and resize functions ** */
+
+inline
+void client_set_geom(client_t *c, const int16_t x, const int16_t y, const uint16_t w, const uint16_t h)
+{
+    window_move_resize(c->win, c->geom.x = x, c->geom.y = y, c->geom.width = w, c->geom.height = h);
+}
+
+inline
+void client_set_geom_geom(client_t *c, const xcb_rectangle_t geom)
+{
+    window_move_resize_geom(c->win, c->geom = geom);
+}
+
+void toggle_fullscreen(client_t *c)
+{
+    c->is_fullscrn = !c->is_fullscrn;
+    xcb_atom_t values[] = { c->is_fullscrn ? cfg.ewmh->_NET_WM_STATE_FULLSCREEN : XCB_NONE };
+    xcb_ewmh_set_wm_state(cfg.ewmh, c->win, LENGTH(values), values);
+
+    if (c->is_fullscrn) {
+        window_set_border_width(c->win, 0);
+        window_move_resize_geom(c->win, c->mon->geom);
+    }
+}
+
+bool window_update_geom(const xcb_window_t win, xcb_rectangle_t *geom)
+{
+    const xcb_get_geometry_cookie_t cookie = xcb_get_geometry(cfg.conn, win);
+    xcb_get_geometry_reply_t *reply = xcb_get_geometry_reply(cfg.conn, cookie, (void *)0);
+
+    if (!reply)
+        return false;
+
+    geom->x = reply->x;
+    geom->y = reply->y;
+    geom->width = reply->width;
+    geom->height = reply->height;
+
+    free(reply);
+
+    return true;
 }
 
