41d9463ed97dda1368956396f40e57b1cb9f28e9
Ivan Kanakarakis <ivan.kanak@gmail.com>
2012-11-19 03:07:21 +0200

    main: add usage

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 #include "main.h"
+
+int usage(void) {
+    puts("man " WM_NAME " # RTFM!");
+    return EXIT_FAILURE;
+}
 
 int main(int argc, char *argv[]) {
     if (argc == 1)
         return cookiewm();
+    else if (!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h"))
+        return usage();
     else
         return cookie(argc, argv);
 }
